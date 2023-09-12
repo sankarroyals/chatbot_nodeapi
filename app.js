419c3f4a8568441620348380f80e4f9156cb4d62
Sankar Yalamandla <sankar.yalamandla@tigeranalytics.local>
2023-09-12 20:57:34 +0530

    Token authentication completed

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fetchingDataRouter = require('./routes/fetchingDataRouter')
+const authRouter = require('./routes/authRouter')
 const {verifyAccessToken} = require('./helpers/jwt_helpers')
 
 const cors = require("cors");
@@ -11,7 +12,9 @@ const app = express();
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 // ROUTES
+app.use("/api/auth",  authRouter);
 app.use("/api/data", verifyAccessToken, fetchingDataRouter);
 module.exports = app;
