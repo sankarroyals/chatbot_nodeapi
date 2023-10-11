8c278902c43ce6648b0d4fcd2f83bbe87e4dd4d0
Rafi.Shaik <rafi.shaik@tigeranalytics.com>
2023-10-11 19:32:31 +0530

    made openai chat from text to group chat

--- controllers/chatBotController.js
+++ controllers/chatBotController.js
@@ -9,11 +9,12 @@ const deploymentName = process.env.OPENAI_DEPLOYMENTNAME;
 
 exports.talk = async (req, res) => {
     try {
-      const {question, type} = req.body;
+      const {questions, type} = req.body;
       if(type=='text'){
-        const { choices } = await client.getCompletions(deploymentName, [question]);
+        const { choices } = await client.getChatCompletions(deploymentName, questions, {maxToken: 2500});
         const completion = choices[0].text;
-        return res.status(200).json({message: completion});
+        questions?.push(choices[0].message);
+        return res.status(200).json({message: questions});
        } else if(type=='image'){
         const size = "256x256";
         const n = 3;
